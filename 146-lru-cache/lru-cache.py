7eb388ef79ee024cf151f6cacdc8d8f9841831dd
rkumar1901 <sureshnadevi1954@gmail.com>
2026-08-18 19:09:48 -0700

    Time: 23 ms (99.01%) | Memory: 29.4 MB (97.74%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,33 +1,75 @@
-class LRUCache:
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.dic = {}
+        
+        self.head = Node()
+        self.tail = Node()
 
-    def get(self, key: int) -> int:
-        
-        if key in self.dic:
-            val = self.dic.pop(key)
-            self.dic[key] = val
-            return val
-        
-        return -1
-        
-    def put(self, key: int, value: int) -> None:
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+
+        next_node = node.next
+        prev_node = node.prev
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def insert_at_end(self,node):
+
+        prev_node = self.tail.prev
+
+        node.next = self.tail
+        prev_node.next = node
+        node.prev = prev_node
+        self.tail.prev = node
+
+    
+    def get(self, key):
+
+        if key not in self.dic:
+            return -1
+
+        node = self.dic[key]
+        self.remove(node)
+        self.insert_at_end(node)
+        return node.value
+
+
+    def put(self, key, value):
 
         if key in self.dic:
-            self.dic.pop(key)
-            self.dic[key] = value
+            node = self.dic[key]
+            node.value = value
+            self.remove(node)
+            self.insert_at_end(node)
             return
 
-        if self.capacity > 0:
-            self.dic[key] = value
-            self.capacity -= 1
+        new_node = Node(key,value)
+        self.insert_at_end(new_node)
+        self.dic[key] = new_node
+        
+        if len(self.dic) > self.capacity:
+            del_node = self.head.next
+            self.remove(del_node)
+            del self.dic[del_node.key]
 
-        else:
-            removeKey = next(iter(self.dic))
-            self.dic.pop(removeKey)
-            self.dic[key] = value
+            
+
+
+
+
+
 
 
 
