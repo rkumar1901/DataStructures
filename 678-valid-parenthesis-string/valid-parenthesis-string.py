d3c45568d8df500251d606edf23e6566a7348e33
rkumar1901 <sureshnadevi1954@gmail.com>
2026-09-12 11:04:23 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (62.14%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        low = 0
+        high = 0
+
+        for char in s:
+
+            if char == '(':
+                low += 1
+                high += 1
+
+            elif char == ')':
+                low -= 1
+                high -= 1
+
+            else:  # '*'
+                low -= 1
+                high += 1
+
+            low = max(0, low)
+
+            if high < 0:
+                return False
+
+        return low == 0
+        
